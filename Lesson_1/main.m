5761e4d74d789cb6ec9e12f9cd1378ed45d42086
Nikita Vintonovich <nikitavintonovich@MacBook-Pro-Nikita.local>
2017-08-08 00:23:53 +0300

    Добаили проект в репозиторий

---
+++ Lesson_1/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Lesson_1
+//
+//  Created by Nikita Vintonovich on 8/7/17.
+//  Copyright © 2017 Vint-Rock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool
+    {
+        // insert code here...
+        NSLog(@"я буду iOS девелопером!");
+        NSLog(@"Привет");
+    }
+    return 0;
+}
